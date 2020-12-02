fdd69a2b61dbcb91d9916a15d128f9f61bb7c81d
Maxim <maxshaptala@gmail.com>
2020-12-02 12:32:51 +0200

    Добавлен пример работы с кнопками, метками, полями ввода и др. компонентами

--- src/main/java/org/itstep/App.java
+++ src/main/java/org/itstep/App.java
@@ -4,9 +4,18 @@ import javafx.animation.FadeTransition;
 import javafx.animation.ScaleTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -22,17 +31,56 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        ImageView logoView = new ImageView();
+//        ImageView logoView = new ImageView();
+//
+//        Image image = new Image(pathToCard("itstep-logo.png"));
+//
+//        logoView.setImage(image);
+//        logoView.setPreserveRatio(true);
+//        logoView.setFitWidth(100);
+//
+//        playAnimation(logoView);
+        // корневой узел
+        Pane rootNode = new Pane();
 
-        Image image = new Image(pathToCard("itstep-logo.png"));
+        // метка - текст
+        Label label = new Label("Hello World");
+        label.setFont(Font.font("Arial", 25));
+        label.setBackground(new Background(new BackgroundFill(Paint.valueOf("red"), null, null)));
+        label.setTextFill(Paint.valueOf("white"));
+        label.relocate(10, 10); // указываем местоположение метки - отступ по 10px сверху и слева
 
-        logoView.setImage(image);
-        logoView.setPreserveRatio(true);
-        logoView.setFitWidth(100);
+        // изображение
+        Image image = new Image(pathToCard("cards/ace_of_clubs.png")); // загружаем изображение
+        ImageView imageView = new ImageView(image);
+        imageView.relocate(200, 10);
+        imageView.setPreserveRatio(true); // сохраняем пропорции
+        imageView.setFitWidth(100); // задаем размер изображения
 
-        playAnimation(logoView);
+        // поле ввода
+        TextField textField = new TextField();
+        textField.relocate(10, 80);
 
-        Scene scene = new Scene(new StackPane(logoView), 640, 480);
+        // кнопки
+        Button button = new Button("Push me");
+        button.relocate(10, 130);
+        button.setOnAction((event) -> label.setText(textField.getText()));
+
+        // прямоугольник
+        Rectangle rectangle = new Rectangle(150, 50);
+        rectangle.setFill(Paint.valueOf("green"));
+
+        // добавить метку в корневой узел
+        rootNode.getChildren().add(rectangle);
+        rootNode.getChildren().add(label);
+        rootNode.getChildren().add(imageView);
+        rootNode.getChildren().add(button);
+        rootNode.getChildren().add(textField);
+
+        // создать сцену
+        Scene scene = new Scene(rootNode, 640, 480);
+
+        // установить сцену в окне
         stage.setScene(scene);
         stage.setTitle("Simple application in JavaFX");
         stage.getIcons().add(new Image("icon.png"));
